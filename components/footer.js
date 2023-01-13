00714b3ef250d0acfaf91fc93a8750e5b983d702
aondenet-sinval <sinvalsatv@gmail.com>
2023-01-13 10:28:12 -0300

    link footer

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.css'
 const Footer = () => {
   return(<>
       <footer className="w3-footer w3-dark-grey w3-padding-small">
-        <h1 className="w3-large">Sinval Gomes Desenvolvimento <a href="https//www.aondenet.com">WEB</a> - Acessibilidade.</h1>
+        <h1 className="w3-large">Sinval Gomes Desenvolvimento <a href="https://www.aondenet.com" target="_blank">WEB</a> - Acessibilidade.</h1>
       </footer>
     </>
   )
